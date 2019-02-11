385cce3c054716dd447a2e964d7acbb948ce9c5a
Adriantorres <adrianalbertotorres@gmail.com>
2019-02-11 15:42:35 -0300

    Contacto

---
+++ appMensaje/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MessageService } from './services/message.service';
+import * as swal from 'sweetalert';
+@Component({
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ constructor(public _MessageService: MessageService) {
+}
+contactForm(form) {
+  this._MessageService.sendMessage(form).subscribe(() => {
+    console.log("Mensaje enviado");
+  });
+  }
+ }
